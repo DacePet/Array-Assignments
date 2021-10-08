3a7bbdd315ea395d79094e5a230bf7d167c9a70f
bootcamp <www.dace.petersone@gmail.com>
2021-10-09 01:36:36 +0300

    3rd commit, Array2 finished (last method not 100% ok). Array3 ignore.

---
+++ src/com/company/Array3.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.Console;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Array3 {
+    public static void main(String[] args) {
+        System.out.println("Please enter 10 sudents results (grades) in range 0-10");
+        Scanner sc = new Scanner(System.in);
+        byte grades = sc.nextByte();
+        byte[] arrGrades = new byte [10];
+        for (int i = 0; i< arrGrades.length; i++) {
+
+            //String userInput = sc.next();
+            //grades[i] =userInput;
+
+        }
+
+
+       // int i = 0;
+//while(i<arrGrades.length){
+       //   System.out.println("Please enter 10 sudents results (grades) in range 0-10");
+          //i++;
+        //}
+          //for (int i = 0; i < arrGrades.length; i++)
+
+    }
+}
