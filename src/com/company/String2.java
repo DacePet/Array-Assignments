cbf983962f966b2935dd21c0fc8d92887456489b
bootcamp <www.dace.petersone@gmail.com>
2021-10-16 22:50:13 +0300

    String 4. 1/4 done.

--- src/com/company/String2.java
+++ src/com/company/String2.java
@@ -1,5 +1,4 @@
 package com.company;
-
 //import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 import javax.swing.*;
@@ -24,7 +23,7 @@ public class String2 {
 
         //3
         int counter = 0;
-        for (int i = 0;i < arrName.length; i++)
+        for (int i = 0; i < arrName.length; i++)
             if (arrName[i].contains("s"))
                 counter++;
         System.out.println(counter + " words consist letter 's'.");
@@ -32,70 +31,23 @@ public class String2 {
         //4.
         System.out.println("Multiple times used words in this sentence: ");
         System.out.println("Solution in progress");
-        // int countcount = 0;
-        //sentence = sentence.toLowerCase();
-        //String wordsDublicates[] = sentence.split(" ");
-
-             //int repeat = 0;
-        //for (int i = 0; i<sentence.length(); i++) {
-          //  for (int j = 0; j < sentence.length(); j++ ) {
-            //    if(sentence.(i)==sentence.charAt(j))
-              //  {
-                //    repeat++;
-                //}
-            //}
-            //System.out.println(sentence.charAt(i)+ "--" +repeat);
-            //String d = String.valueOf(sentence(i)).trim();
-
-            //repeat = 0;
-        //}
-        //for (int i = 0; i < arrName.length; i++) {
-          //  countcount = 1;
-            //for (int j = i + 1; j < arrName.length; j++) {
-              //  if (arrName[i].equals(arrName[j])) {
-                //    countcount++;
-                  //  arrName[j] = "0";
-               // }
-               // if (countcount > 1 && arrName[i] != "0")
-                 //   System.out.println(arrName[i]);
-           // }
-      //  }
-
-
 //String findEquals =
-        //int i = 0;
-        //Pattern pattern = Pattern.compile(arrName[i]);
-        //Matcher matcher = pattern.matcher(arrName[i]);
-        ///boolean matchFound = matcher.find();
-        //if (matchFound) {
-        //  System.out.println("Found");
-        //}else {
-        //  System.out.println("No word is repeated multiple times in the sentence");
+//int i = 0;
+//String regex = "Climb mountains not so the world can see you, but so you can see the world"
+        for (String token : arrName) {
+            String regex = token;
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(sentence);
+            int count2 = 0;
+            while (matcher.find()) {
 
-//        while (matcher.find())
-        // System.out.println("Multiple: ");
-        //int countcount = 0;
-        //for (int i = 0; i < arrName.length; i++) {
-        //  if (arrName[i].contentEquals()) {
-        //    i++;
-        //  countcount++;
-        // System.out.println("Any result? " + i);
-//            System.out.println("Word " + i + " is repeated " + countcount + " times in the sentence.");
-        // System.out.println(sentence[i]);
-        //TODO
-        // int i = 0;
+                System.out.println(regex);
+               // System.out.println("The word '" + regex + "' is used  " + count2 + " times.");
 
-
-        // int i = 0;
-//while (arrName[i[.co (StringBuffer arrName [i]);
-//System.out.println("Words " + arrName[i] + " are repeated multiple times in the sentence.");
-//StringBuilder builder = new StringBuilder(sentence);
-        //  System.out.println(sentence.equals(builder));
-        //System.out.println(sentence.contentEquals(builder));
-
-
-        //5.
-        String replaceString = sentence.replace("you", "You");
-        System.out.println(replaceString);
+            }
+        }
+            //5.
+            String replaceString = sentence.replace("you", "You");
+            System.out.println(replaceString);
+        }
     }
-}
--- src/com/company/String4.java
+++ src/com/company/String4.java
@@ -1,13 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
 public class String4 {
     public enum Suit {SPADES, CLUBS, HEARTS, DIAMONDS}
+
     public enum Values {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
 
     public static void main(String[] args) {
-for (int i = 0; i<Suit.values().length; i++) {
-    for (int j = 0; j < Values.values().length; j++)
-        System.out.println(Suit.values() + " " + Values.values());
-}
+
+
+        for (Suit suit : Suit.values()) {
+            for (Values values : Values.values())
+                System.out.println(suit + " " + values);
+            System.out.println();
+
+
+
+        }
     }
+    //public static void shuffle(){
+      //  private static final List <Suit> CARDS = new ArrayList<>(Arrays.asList(Suit.values()))
+        //Collections,shuffle(CARDS)
+          //      return CARDS.remove(0);
+
+
 }
